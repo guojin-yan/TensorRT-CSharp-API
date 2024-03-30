f1371f88388b9bd5280f7b4287f1b239a19b2ce2
yanguojin <guojin_yjs@cumt.edu.cn>
2024-03-30 16:09:31 +0800

    Ad ITrtPtrHolder interface.

---
+++ src/TensorRtSharp/Fundamentals/ITrtPtrHolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TensorRtSharp
+{
+    /// <summary>
+    /// Represents a TensorRT-based class which has a native pointer. 
+    /// </summary>
+    public interface ITrtPtrHolder
+    {
+        /// <summary>
+        /// Unmanaged TensorRT data pointer
+        /// </summary>
+        IntPtr TrtPtr { get; }
+    }
+}
